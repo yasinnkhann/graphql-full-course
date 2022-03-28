2491917c21abddd3ba72ade0a85e73a1fa4993a2
Yasin Khan <yasinkhan500@gmail.com>
2022-03-28 13:14:58 -0700

    stache

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,42 @@
 import './App.css';
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink,
-  from,
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	HttpLink,
+	from,
 } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 import DisplayData from './DisplayData';
 
-const errorLink = onError(({ graphqlErrors, networkError }) => {
-  if (graphqlErrors) {
-    graphqlErrors.map(({ message, location, path }) => {
-      return alert(`Graphql error ${message}`);
-    });
-  }
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors)
+		graphQLErrors.forEach(err => {
+			const { locations, message, path } = err;
+			console.warn(
+				`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+			);
+		});
 });
 
 const link = from([
-  errorLink,
-  new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+	errorLink,
+	new HttpLink({ uri: 'http://localhost:4000/graphql' }),
 ]);
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: link,
+	cache: new InMemoryCache(),
+	link: link,
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div className='App'>
-        <DisplayData />
-      </div>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div className='App'>
+				<DisplayData />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
