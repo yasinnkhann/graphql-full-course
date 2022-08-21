1507c41b6aa825ae1dcd542a5ff50956873e1c76
Yasin Khan <yasinkhan500@gmail.com>
2022-08-21 03:02:57 -0700

    stache

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -2,158 +2,209 @@ import React, { useState } from 'react';
 import { useQuery, useLazyQuery, gql, useMutation } from '@apollo/client';
 
 const QUERY_ALL_USERS = gql`
-  query GetAllUsers {
-    users {
-      id
-      name
-      age
-      username
-      nationality
-    }
-  }
+	query GetAllUsers {
+		users {
+			id
+			name
+			age
+			username
+			nationality
+		}
+	}
 `;
 
 const QUERY_ALL_MOVIES = gql`
-  query GetAllMovies {
-    movies {
-      name
-    }
-  }
+	query GetAllMovies {
+		movies {
+			name
+		}
+	}
 `;
 
 const GET_MOVIE_BY_NAME = gql`
-  query Movie($name: String!) {
-    movie(name: $name) {
-      name
-      yearOfPublication
-    }
-  }
+	query Movie($name: String!) {
+		movie(name: $name) {
+			name
+			yearOfPublication
+		}
+	}
 `;
 
 const CREATE_USER_MUTATION = gql`
-  mutation CreateUser($input: CreateUserInput!) {
-    createUser(input: $input) {
-      name
-      id
-    }
-  }
+	mutation CreateUser($input: CreateUserInput!) {
+		createUser(input: $input) {
+			name
+			id
+		}
+	}
+`;
+
+const DELETE_USER_MUTATION = gql`
+	mutation DeleteUser($id: ID!) {
+		deleteUser(id: $id) {
+			name
+		}
+	}
+`;
+
+const UPDATE_USERNAME_MUTATION = gql`
+	mutation UpdateUsername($input: UpdateUsernameInput!) {
+		updateUsername(input: $input) {
+			id
+			name
+		}
+	}
 `;
 
 function DisplayData() {
-  const [movieSearched, setMovieSearched] = useState('');
+	const [movieSearched, setMovieSearched] = useState('');
 
-  // Create User States
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
-  const [age, setAge] = useState(0);
-  const [nationality, setNationality] = useState('');
+	// Create User States
+	const [name, setName] = useState('');
+	const [username, setUsername] = useState('');
+	const [age, setAge] = useState(0);
+	const [nationality, setNationality] = useState('');
 
-  const { data, loading, refetch } = useQuery(QUERY_ALL_USERS);
-  const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
-  const [fetchMovie, { data: movieSearchedData, error: movieError }] =
-    useLazyQuery(GET_MOVIE_BY_NAME);
+	const { data, loading, refetch } = useQuery(QUERY_ALL_USERS);
+	const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
+	const [fetchMovie, { data: movieSearchedData, error: movieError }] =
+		useLazyQuery(GET_MOVIE_BY_NAME);
 
-  const [createUser] = useMutation(CREATE_USER_MUTATION);
+	const [createUser] = useMutation(CREATE_USER_MUTATION);
 
-  if (loading) {
-    return <h1> DATA IS LOADING...</h1>;
-  }
+	const [deleteUser] = useMutation(DELETE_USER_MUTATION);
 
-  return (
-    <div>
-      <div>
-        <input
-          type='text'
-          placeholder='Name...'
-          onChange={event => {
-            setName(event.target.value);
-          }}
-        />
-        <input
-          type='text'
-          placeholder='Username...'
-          onChange={event => {
-            setUsername(event.target.value);
-          }}
-        />
-        <input
-          type='number'
-          placeholder='Age...'
-          onChange={event => {
-            setAge(event.target.value);
-          }}
-        />
-        <input
-          type='text'
-          placeholder='Nationality...'
-          onChange={event => {
-            setNationality(event.target.value.toUpperCase());
-          }}
-        />
-        <button
-          onClick={() => {
-            createUser({
-              variables: {
-                input: { name, username, age: Number(age), nationality },
-              },
-            });
+	const [updateUsername] = useMutation(UPDATE_USERNAME_MUTATION);
 
-            refetch();
-          }}
-        >
-          Create User
-        </button>
-      </div>
-      {data &&
-        data.users.map(user => {
-          return (
-            <div>
-              <h1>Name: {user.name}</h1>
-              <h1>Username: {user.username}</h1>
-              <h1>Age: {user.age}</h1>
-              <h1>Nationality: {user.nationality}</h1>
-            </div>
-          );
-        })}
+	if (loading) {
+		return <h1> DATA IS LOADING...</h1>;
+	}
 
-      {movieData &&
-        movieData.movies.map(movie => {
-          return <h1>Movie Name: {movie.name}</h1>;
-        })}
+	return (
+		<div>
+			<div>
+				<input
+					type='text'
+					placeholder='Name...'
+					onChange={event => {
+						setName(event.target.value);
+					}}
+				/>
+				<input
+					type='text'
+					placeholder='Username...'
+					onChange={event => {
+						setUsername(event.target.value);
+					}}
+				/>
+				<input
+					type='number'
+					placeholder='Age...'
+					onChange={event => {
+						setAge(event.target.value);
+					}}
+				/>
+				<input
+					type='text'
+					placeholder='Nationality...'
+					onChange={event => {
+						setNationality(event.target.value.toUpperCase());
+					}}
+				/>
+				<button
+					onClick={() => {
+						createUser({
+							variables: {
+								input: { name, username, age: Number(age), nationality },
+							},
+						});
 
-      <div>
-        <input
-          type='text'
-          placeholder='Interstellar...'
-          onChange={event => {
-            setMovieSearched(event.target.value);
-          }}
-        />
-        <button
-          onClick={() => {
-            fetchMovie({
-              variables: {
-                name: movieSearched,
-              },
-            });
-          }}
-        >
-          Fetch Data
-        </button>
-        <div>
-          {movieSearchedData && (
-            <div>
-              <h1>MovieName: {movieSearchedData.movie.name}</h1>
-              <h1>
-                Year Of Publication: {movieSearchedData.movie.yearOfPublication}
-              </h1>{' '}
-            </div>
-          )}
-          {movieError && <h1> There was an error fetching the data</h1>}
-        </div>
-      </div>
-    </div>
-  );
+						refetch();
+					}}
+				>
+					Create User
+				</button>
+			</div>
+			{data &&
+				data.users.map(user => {
+					return (
+						<div key={user.id}>
+							<h1>ID: {user.id}</h1>
+							<h1>Name: {user.name}</h1>
+							<h1>Username: {user.username}</h1>
+							<h1>Age: {user.age}</h1>
+							<h1>Nationality: {user.nationality}</h1>
+							<button
+								onClick={() => {
+									deleteUser({
+										variables: {
+											id: user.id,
+										},
+									});
+
+									refetch();
+								}}
+							>
+								Delete User
+							</button>
+							<button
+								onClick={() => {
+									updateUsername({
+										variables: {
+											input: {
+												id: user.id,
+												newUsername: 'New Username',
+											},
+										},
+									});
+
+									refetch();
+								}}
+							>
+								Update User
+							</button>
+						</div>
+					);
+				})}
+
+			{movieData &&
+				movieData.movies.map(movie => {
+					return <h1 key={movie.name}>Movie Name: {movie.name}</h1>;
+				})}
+
+			<div>
+				<input
+					type='text'
+					placeholder='Interstellar...'
+					onChange={event => {
+						setMovieSearched(event.target.value);
+					}}
+				/>
+				<button
+					onClick={() => {
+						fetchMovie({
+							variables: {
+								name: movieSearched,
+							},
+						});
+					}}
+				>
+					Fetch Data
+				</button>
+				<div>
+					{movieSearchedData && (
+						<div>
+							<h1>MovieName: {movieSearchedData.movie.name}</h1>
+							<h1>
+								Year Of Publication: {movieSearchedData.movie.yearOfPublication}
+							</h1>{' '}
+						</div>
+					)}
+					{movieError && <h1> There was an error fetching the data</h1>}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default DisplayData;
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -2,66 +2,67 @@ const { UserList, MovieList } = require('../FakeData');
 const _ = require('lodash');
 
 const resolvers = {
-  Query: {
-    // USER RESOLVERS
-    users: (parent, args, context, info) => {
-      // console.log(context.req);
-      return UserList;
-    },
-    user: (parent, args) => {
-      const id = args.id;
-      const user = _.find(UserList, { id: Number(id) });
-      return user;
-    },
+	Query: {
+		// USER RESOLVERS
+		users: (parent, args, context, info) => {
+			// console.log(context.req);
+			return UserList;
+		},
+		user: (parent, args) => {
+			const id = args.id;
+			const user = _.find(UserList, { id: Number(id) });
+			return user;
+		},
 
-    // MOVIE RESOLVERS
-    movies: () => {
-      return MovieList;
-    },
-    movie: (parent, args) => {
-      const name = args.name;
-      const movie = _.find(MovieList, { name });
-      return movie;
-    },
-  },
-  User: {
-    favoriteMovies: () => {
-      return _.filter(
-        MovieList,
-        movie =>
-          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
-      );
-    },
-  },
+		// MOVIE RESOLVERS
+		movies: () => {
+			return MovieList;
+		},
+		movie: (parent, args) => {
+			const name = args.name;
+			const movie = _.find(MovieList, { name });
+			return movie;
+		},
+	},
+	User: {
+		favoriteMovies: () => {
+			return _.filter(
+				MovieList,
+				movie =>
+					movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+			);
+		},
+	},
 
-  Mutation: {
-    createUser: (parent, args) => {
-      const user = args.input;
-      const lastId = UserList[UserList.length - 1].id;
-      user.id = lastId + 1;
-      UserList.push(user);
-      return user;
-    },
+	Mutation: {
+		createUser: (parent, args) => {
+			const user = args.input;
+			const lastId = UserList[UserList.length - 1].id;
+			user.id = lastId + 1;
+			UserList.push(user);
+			return user;
+		},
 
-    updateUsername: (parent, args) => {
-      const { id, newUsername } = args.input;
-      let userUpdated;
-      UserList.forEach(user => {
-        if (user.id === Number(id)) {
-          user.username = newUsername;
-          userUpdated = user;
-        }
-      });
+		updateUsername: (parent, args) => {
+			const { id, newUsername } = args.input;
+			let userUpdated;
+			UserList.forEach(user => {
+				if (user.id === Number(id)) {
+					user.username = newUsername;
+					userUpdated = user;
+				}
+			});
 
-      return userUpdated;
-    },
+			return userUpdated;
+		},
 
-    deleteUser: (parent, args) => {
-      const id = args.id;
-      _.remove(UserList, user => user.id === Number(id));
-      return null;
-    },
-  },
+		deleteUser: (parent, args) => {
+			const id = args.id;
+			const deletedUser = _.find(UserList, { id: Number(id) });
+			_.remove(UserList, user => user.id === Number(id));
+			return deletedUser;
+		},
+	},
 };
 
 module.exports = { resolvers };
